37b9780d4e655097545360cb11a9bdd8bc07883f
ConnorDY <cdy@email.sc.edu>
2015-03-21 21:10:57 -0400

    Animated obelisk flame.

--- Platformer/obelisk.cpp
+++ Platformer/obelisk.cpp
@@ -6,12 +6,35 @@ Obelisk::Obelisk(TextureManager &textureManager, float x, float y, float height)
 	)
 {
 	spr.setTexture(textureManager.getRef("obelisk"));
+	
+	flames.push_back(sf::IntRect(30, 8, 7, 8));
+	flames.push_back(sf::IntRect(30, 0, 7, 8));
+	flames.push_back(sf::IntRect(30, 8, 7, 8));
 }
 
 
 /* Actions */
+void Obelisk::update(sf::Time deltaTime, Room const &room, const settings_t &settings)
+{
+	Object::update(deltaTime, room, settings);
+
+	// Flame Animation
+	frame += dir * deltaTime.asSeconds() * 3;
+}
+
 void Obelisk::draw(sf::RenderWindow &window)
 {
+	// Draw Obelisk
+	spr.setTextureRect(sf::IntRect(0, 0, 30, 72));
 	spr.setPosition(sf::Vector2f(x, y - 1)); // "y - 1" as an easy collision fix of sorts
 	window.draw(spr);
+
+	int f = (int)frame % 4;
+
+	if (f > 0)
+	{
+		spr.setTextureRect(flames.at(f - 1));
+		spr.setPosition(sf::Vector2f(x + 13, y + 17));
+		window.draw(spr);
+	}
 }
--- Platformer/obelisk.h
+++ Platformer/obelisk.h
@@ -8,13 +8,17 @@
 
 class Obelisk : public Object
 {
-private:
-	sf::Sprite spr;
-public:
-	explicit Obelisk(TextureManager &textureManager, float x = -128, float y = -128, float height = 72);
+	private:
+		sf::Sprite spr;
+		std::vector<sf::IntRect> flames;
+		double frame = 0;
+		int dir = 1;
+	public:
+		explicit Obelisk(TextureManager &textureManager, float x = -128, float y = -128, float height = 72);
 
-	// Actions
-	virtual void draw(sf::RenderWindow &window);
+		// Actions
+		virtual void update(sf::Time deltaTime, Room const &room, const settings_t &settings);
+		virtual void draw(sf::RenderWindow &window);
 };
 
 #endif
