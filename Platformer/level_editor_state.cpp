fa01b3123a84fd46a4f2f5565b5ea0d4f7be8206
ConnorDY <cdy@email.sc.edu>
2015-04-02 19:46:01 -0400

    Level editor progress.

--- Platformer/level_editor_state.cpp
+++ Platformer/level_editor_state.cpp
@@ -1,11 +1,23 @@
 #include "level_editor_state.h"
 #include "menu_state.h"
 
+#define GRID_SCALE 8
+
 Level_Editor_State::Level_Editor_State(StateManager &sM, SoundManager &som, TextureManager &textureManager, const settings_t &settings)
 	: Room(sM, som, textureManager, settings)
 {
-	point = sf::Vector2i(0, 0);
+	cursor = sf::Vector2i(0, 0);
+	point = cursor;
 	clicked = false;
+
+	shapeCursor.setSize(sf::Vector2f(2, 2));
+	shapeCursor.setOrigin(sf::Vector2f(1, 1));
+	shapeCursor.setFillColor(sf::Color(255, 0, 0, 180));
+
+	shapeGrid.setSize(sf::Vector2f(GRID_SCALE - 2, GRID_SCALE - 2));
+	shapeGrid.setOrigin(sf::Vector2f(-1, -1));
+	shapeGrid.setFillColor(sf::Color(255, 255, 255, 10));
+	shapeGrid.setOutlineThickness(1);
 }
 
 Level_Editor_State::~Level_Editor_State()
@@ -15,6 +27,28 @@ Level_Editor_State::~Level_Editor_State()
 void Level_Editor_State::start(TextureManager &textureManager, const settings_t &settings)
 {
 
+}
+
+void Level_Editor_State::drawForeground(sf::RenderWindow &window)
+{
+	// Grid
+	for (unsigned int i = 0; i <= ceil(VIEW_WIDTH / GRID_SCALE); i++)
+	{
+		for (unsigned int j = 0; j <= ceil(VIEW_HEIGHT / GRID_SCALE); j++)
+		{
+			shapeGrid.setOutlineColor(sf::Color(255, 255, 255, 50));
+			shapeGrid.setPosition(sf::Vector2f(i * GRID_SCALE, j * GRID_SCALE));
+			window.draw(shapeGrid);
+		}
+	}
+
+	// Heightmap
+	Room::drawForeground(window);
+
+	// Highlighted Grid Part
+	shapeGrid.setOutlineColor(sf::Color(0, 216, 216, 100));
+	shapeGrid.setPosition(sf::Vector2f(gridCursor.x * GRID_SCALE, gridCursor.y * GRID_SCALE));
+	window.draw(shapeGrid);
 }
 
 void Level_Editor_State::update(sf::RenderWindow &window, TextureManager &textureManager, SoundManager &soundManager, InputHandler &inputHandler, settings_t &settings)
@@ -46,16 +80,21 @@ void Level_Editor_State::update(sf::RenderWindow &window, TextureManager &textur
 		{
 			if (!clicked)
 			{
-				point = sf::Mouse::getPosition(window);
+				point = gridCursor;
 				clicked = true;
 			}
 		}
 		else if (clicked)
 		{
-			fillHeightMap(point.x / settings.window_scale, (sf::Mouse::getPosition(window).x - point.x) / settings.window_scale, abs(VIEW_HEIGHT - (point.y / settings.window_scale)));
+			fillHeightMap(point.x * GRID_SCALE, (gridCursor.x + 1 - point.x) * GRID_SCALE, abs(VIEW_HEIGHT - (point.y * GRID_SCALE)));
 			clicked = false;
 		}
 	}
+
+	// Get mouse position
+	sf::Vector2i m = sf::Mouse::getPosition(window);
+	cursor = sf::Vector2i(floor(m.x / (float)settings.window_scale), floor(m.y / (float)settings.window_scale));
+	gridCursor = sf::Vector2i(floor(cursor.x / GRID_SCALE), floor(cursor.y / GRID_SCALE));
 
 	Room::update(window, textureManager, soundManager, inputHandler, settings);
 }
--- Platformer/level_editor_state.h
+++ Platformer/level_editor_state.h
@@ -18,7 +18,8 @@ class Level_Editor_State : public Room
 {
 	private:
 		sf::Clock restartTimer;
-		sf::Vector2i point;
+		sf::Vector2i cursor, gridCursor, point;
+		sf::RectangleShape shapeCursor, shapeGrid;
 		bool clicked;	
 	public:
 		Level_Editor_State(StateManager &sM, SoundManager &som, TextureManager &textureManager, const settings_t &settings);
@@ -26,6 +27,7 @@ class Level_Editor_State : public Room
 
 		// Actions
 		void start(TextureManager &textureManager, const settings_t &settings);
+		virtual void drawForeground(sf::RenderWindow &window);
 		virtual void update(sf::RenderWindow &window, TextureManager &textureManager, SoundManager &soundManager, InputHandler &inputHandler, settings_t &settings);
 };
 
