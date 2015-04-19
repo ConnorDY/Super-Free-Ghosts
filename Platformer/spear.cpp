3f92a75202d39c8d65545643f1a3099097f80725
Score_Under <seejay.11@gmail.com>
2015-04-19 01:12:13 +0100

    Split Spear constructor to allow easier subclassing

--- Platformer/spear.cpp
+++ Platformer/spear.cpp
@@ -2,22 +2,23 @@
 #include "sprite_util.h"
 
 namespace {
+	// Normal spear: Sprite strip of 49x12 sprites, 1 frame, BB 35x6+8+3
 	sf::IntRect const BBOX(8, 3, 35, 6);
 	int const SPRITE_WIDTH = 49, SPRITE_HEIGHT = 12;
 }
 
 Spear::Spear(float x, float y, int dir, TextureManager &textureManager)
-	: Weapon(x, y, BBOX.width, BBOX.height, 0.4f * dir, 0, 0, 0, 10, textureManager)
+	: Spear(x, y, BBOX.width, BBOX.height, dir, textureManager)
 {
-	// Normal spear: Sprite strip of 49x12 sprites, 1 frame, BB 35x6+8+3
-	sprite.setTexture(textureManager.getRef("spear"));
 	animationFrames.emplace_back(0, 0, SPRITE_WIDTH, SPRITE_HEIGHT);
-	SpriteUtil::setOriginAndScale(sprite,
-			sf::Vector2f(static_cast<float>(dir), 1.0f),
-			BBOX
-	);
+	SpriteUtil::setOriginAndScale(sprite, sf::Vector2f(static_cast<float>(dir), 1.0f), BBOX);
+}
 
+Spear::Spear(float x, float y, int width, int height, int dir, TextureManager &textureManager)
+	: Weapon(x, y, width, height, 0.4f * dir, 0, 0, 0, 10, textureManager)
+{
 	setDepth(-4);
+	sprite.setTexture(textureManager.getRef("spear"));
 }
 
 Spear::~Spear()
--- Platformer/spear.h
+++ Platformer/spear.h
@@ -1,11 +1,13 @@
 #ifndef SPEAR_H
 #define SPEAR_H
 
-#include <SFML/Graphics.hpp>
 #include "weapon.h"
 
 class Spear : public Weapon
 {
+	protected:
+		// This constructor does not set the animation frames, nor the sprite origin/scale. The only change it makes to the sprite is to set its texture. Hence it is protected, because nothing outside of the inheritance hierarchy could use it.
+		Spear(float x, float y, int width, int height, int dir, TextureManager &textureManager);
 	public:
 		Spear(float x, float y, int dir, TextureManager &textureManager);
 		virtual ~Spear();
